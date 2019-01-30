79d90e3f4ea088be4777ad811730ef2968f26eeb
JohnBear2008 <johnbear2008@qq.com>
2019-01-30 15:29:11 +0800

    0.16.2
    执行SQL 转移至后端 增加安全性

--- biz/PM/getSQLDBData.{m}.js
+++ biz/PM/getSQLDBData.{m}.js
@@ -1,21 +1,34 @@
 require("../../client/js/Date.js");
-
+require("../../client/js/SQL.js");
 module.exports = function(sender) {
     var yjDBService = global.yjRequire("yujiang.Foil").yjDBService;
     var yjDB = global.yjRequire("yujiang.Foil").yjDB;
 
 
-    
-   console.log("get:"+JSON.stringify(sender.req.query));
+//    console.log("get:"+JSON.stringify(sender.req.query));
 
     var SQL=sender.req.query.SQL;  
+    
+    console.log("SQL:"+SQL);
+    //选择执行的SQL语句
+    switch (SQL){
+	case "SQLBDMHESystems":
+		var SQLExecute=SQLBDMHESystems;
+		break;
+	case "SQLTableBillPlan":
+		var SQLExecute=SQLTableBillPlan;
+		break;	
+	default:
+		var SQLExecute=SQL;
+		break;
+	}
     
     
     //增加关键字防护,防止使用非法关键字操作数据库
     var banWord1 = new RegExp("delete");
     var banWord2 = new RegExp("update");
     var banWord3 = new RegExp("insert");
-    var resultCheckSQL = banWord1.test(SQL)||banWord2.test(SQL)||banWord3.test(SQL);
+    var resultCheckSQL = banWord1.test(SQLExecute)||banWord2.test(SQLExecute)||banWord3.test(SQLExecute);
     
     
     if(resultCheckSQL){
@@ -23,7 +36,7 @@ module.exports = function(sender) {
     }else{
     	
     	 yjDBService.exec({
-    	        sql: SQL,
+    	        sql: SQLExecute,
     	        parameters: [],
     	        rowsAsArray: false, // Chenly 2018-10-19 返回obj arr
     	        success: function(result) {
---
+++ client/js/SQL.js
@@ -0,0 +1,12 @@
+/**
+ *SQL.js
+ *定义系统中固定的SQL语句,返回指定值
+ */
+
+
+//定义系统常用的SQL语句,全局变量
+SQLBDMHESystems="select DBID,belong,STMType,model, case status  when 1 then '正常' when 2 then '作废'  end as statustext from `pm_systems`";
+
+
+
+SQLTableBillPlan="select tA.*,tB.fileKey,tB.fileName from `pm_bills_plan` tA left join `pm_files_upload` tB on tA.BPID=tB.billID and tB.billName='pm_bills_plan'";
