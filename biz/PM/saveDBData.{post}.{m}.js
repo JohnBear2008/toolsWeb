8290846da514f2f42c09f288d58094f1f2e3359a
John Bear <42626532+JohnBear2008@users.noreply.github.com>
2020-07-10 17:20:42 +0800

    1

--- biz/PM/saveDBData.{post}.{m}.js
+++ biz/PM/saveDBData.{post}.{m}.js
@@ -4,105 +4,101 @@
  * 
  * 
  * */
-module.exports = function(sender) {
+module.exports = function (sender) {
 
-    var yjDBService = global.yjRequire("yujiang.Foil").yjDBService;
-    var yjDB = global.yjRequire("yujiang.Foil").yjDB;
- //   console.log("get:"+JSON.stringify(sender.req.query));
-    
-//   console.log("PM2333 test");
-    var obj=sender.req.query;
-  
-  var DBID=sender.req.query.DBID;
-  
-  if(DBID==""){
-//	    console.log("执行新增");
-//	    console.log("DBID:"+DBID);
+	var yjDBService = global.yjRequire("yujiang.Foil").yjDBService;
+	var yjDB = global.yjRequire("yujiang.Foil").yjDB;
+	//   console.log("get:"+JSON.stringify(sender.req.query));
+	//   console.log("PM2333 test");
+	var obj = sender.req.query;
 
-	    var tableTitle="";
-	    var tableData="";
-	    
-	    for(var key in obj){
-	    	switch(key){
-	    	case "DBID":
-	    		break;
-	    	case "DBTable":
-	    		var DBTable=obj[key];
-	    		break;
-	    	default:
-	    		tableTitle=tableTitle+key+",";
-	    	//增加为空判断,为空则替换为null 防止插入数据库格式类型不对错误
-	  		if(obj[key]==""){
-	  			tableData=tableData+null+",";
-	  		}else{
-	  			tableData=tableData+"'"+obj[key]+"',";
-	  		}
-	  	
-	    	
-	    	}
+	var DBID = sender.req.query.DBID;
 
-	    }
-	    
-	    tableTitle=tableTitle.substr(0, tableTitle.length - 1);  
-	    tableData=tableData.substr(0, tableData.length - 1);  
-	    
+	if (DBID == "") {
+		//	    console.log("执行新增");
+		//	    console.log("DBID:"+DBID);
 
-		
-	var SQLInsert="insert into `"+DBTable+"` ("+tableTitle+") values "+"("+tableData+")";
+		var tableTitle = "";
+		var tableData = "";
 
-//	console.log(SQLInsert)
+		for (var key in obj) {
+			switch (key) {
+				case "DBID":
+					break;
+				case "DBTable":
+					var DBTable = obj[key];
+					break;
+				default:
+					tableTitle = tableTitle + key + ",";
+					//增加为空判断,为空则替换为null 防止插入数据库格式类型不对错误
+					if (obj[key] == "") {
+						tableData = tableData + null + ",";
+					} else {
+						tableData = tableData + "'" + obj[key] + "',";
+					}
 
-//		console.log("SQLInsert:"+SQLInsert);
 
-	yjDBService.exec({
-	    sql: SQLInsert,
-	    parameters: [],
-	    success:  function(result) {
+			}
 
-	    	sender.success(result)
-	    },
-	    error: {},
-	});
+		}
 
-	  
-  }else{
+		tableTitle = tableTitle.substr(0, tableTitle.length - 1);
+		tableData = tableData.substr(0, tableData.length - 1);
 
-//	 console.log("执行更新");
-//	 console.log("DBID:"+DBID);
+		var SQLInsert = "insert into `" + DBTable + "` (" + tableTitle + ") values " + "(" + tableData + ")";
 
-	    var updateContent="";
+		//	console.log(SQLInsert)
+		//		console.log("SQLInsert:"+SQLInsert);
 
-	    
-	    for(var key in obj){ 	
-	    	if(key=="DBTable"){
-	    		var DBTable=obj[key];
-	    	}else if(key=="DBID"){
-	    		var DBID=obj[key];
-	    	}else{
+		yjDBService.exec({
+			sql: SQLInsert,
+			parameters: [],
+			success: function (result) {
 
-	    		//增加为空判断,为空则替换为null 防止插入数据库格式类型不对错误
-	    		if(obj[key]==""){
-	    			updateContent=updateContent+key+"="+null+",";
-	    		}else{
-	    			updateContent=updateContent+key+"="+"'"+obj[key]+"',";
-	    		}
-	    		
-//	    		tableTitle=tableTitle+key+",";
-//	    		tableData=tableData+obj[key]+",";
-	    	}
-	    }
-	      
-	    updateContent=updateContent.substr(0, updateContent.length - 1);  
-	    var SQLUpdate=" update "+DBTable+" set "+updateContent +" where DBID="+DBID;
+				sender.success(result)
+			},
+			error: {},
+		});
 
-	    yjDBService.exec({
-	        sql: SQLUpdate,
-	        parameters: [],
-	        success: sender.success,
-	        error: sender.error
-	    });
-  
-  }
+
+	} else {
+
+		//	 console.log("执行更新");
+		//	 console.log("DBID:"+DBID);
+
+		var updateContent = "";
+
+
+		for (var key in obj) {
+			if (key == "DBTable") {
+				var DBTable = obj[key];
+			} else if (key == "DBID") {
+				var DBID = obj[key];
+			} else {
+
+				//增加为空判断,为空则替换为null 防止插入数据库格式类型不对错误
+				if (obj[key] == "") {
+					updateContent = updateContent + key + "=" + null + ",";
+				} else {
+					updateContent = updateContent + key + "=" + "'" + obj[key] + "',";
+				}
+
+				//	    		tableTitle=tableTitle+key+",";
+				//	    		tableData=tableData+obj[key]+",";
+			}
+		}
+
+		updateContent = updateContent.substr(0, updateContent.length - 1);
+		var SQLUpdate = " update " + DBTable + " set " + updateContent + " where DBID=" + DBID;
+
+		yjDBService.exec({
+			sql: SQLUpdate,
+			parameters: [],
+			success: sender.success,
+			error: sender.error
+		});
+
+	}
 
 
 };
